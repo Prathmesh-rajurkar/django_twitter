3f1f883dde527562121b191743e8d15e727ea37a
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-03-19 19:53:43 +0530

    added requirements.txt

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pi+zu3r9lod8v-%1wj4c$vct@vt^3s366ng)h_zcd+u-3vdopn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 import cloudinary
 import cloudinary.uploader
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,7 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
